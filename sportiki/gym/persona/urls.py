13136a068e35c95ce1558d2acc8348b8e9000dcc
daniel <maduludaniel1@gmail.com>
2025-07-08 16:43:14 +0200

    delet files

--- sportiki/gym/persona/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-ulrlpatterns = [
-    path('profile/', views.profilePage, name='profile'),
-    
-    
-]
--- sportiki/gym/persona/views.py
+++
@@ -1,5 +0,0 @@
-from django.shortcuts import render
-
-def profilePage(request):
-    return render(request, 'persona/profile.html')
-# Create your views here.
